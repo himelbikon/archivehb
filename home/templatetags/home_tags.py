e96943fdc80ac195baa11a5e308ced7f74431a82
himelbikon <skhimelhaque@gmail.com>
2020-12-20 21:00:11 +0600

    banner with slide

---
+++ home/templatetags/home_tags.py
@@ -0,0 +1,17 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def js_list(ten_obj):
+	arr = []
+	for x in ten_obj:
+		question = x.question + ' <br>'
+		a = 'a) ' + x.a + '&nbsp'*3
+		b = 'b) ' + x.b + ' <br>'
+		c = 'c) ' + x.c + '&nbsp'*3
+		d = 'd) ' + x.d + ' <br>'
+		answer = 'Answer: ' + x.answer + ' <br>'
+		explanation = 'Explanation: ' + x.explanation[0:100] + '.....' + ' '*50
+		arr.append(question + a + b + c + d + answer + explanation)
+	return 'var arr = ' + str(arr) + ';'
--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from quiz.models import HSC_Quiz
 #from django.http import HttpResponse
 
 # home views
 
 def home(request):
-    return render(request, 'home/home.html')
+    ten_obj = HSC_Quiz.objects.all()
+    return render(request, 'home/home.html', {'ten_obj':ten_obj})
