f993ce28d64d239bc8c46ca6bd65483fbbef3cef
himelbikon <skhimelhaque@gmail.com>
2020-12-21 15:30:46 +0600

    front block design completed

--- home/templatetags/home_tags.py
+++ home/templatetags/home_tags.py
@@ -8,10 +8,10 @@ def js_list(ten_obj):
 	for x in ten_obj:
 		question = x.question + ' <br>'
 		a = 'a) ' + x.a + '&nbsp'*3
-		b = 'b) ' + x.b + '&nbsp'*3
+		b = 'b) ' + x.b + ' <br>'
 		c = 'c) ' + x.c + '&nbsp'*3
 		d = 'd) ' + x.d + ' <br>'
 		answer = 'Answer: ' + x.answer + ' <br>'
-		explanation = 'Explanation: ' + x.explanation[0:100] + '.....' + ' '*50
+		explanation = 'Explanation: ' + x.explanation[0:100] + '.....' + ' '*150
 		arr.append(question + a + b + c + d + answer + explanation)
 	return 'var arr = ' + str(arr) + ';'
