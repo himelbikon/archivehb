e0c755a4176cf7866c6bc56c004bcd5f21de9224
himelbikon <skhimelhaque@gmail.com>
2020-12-27 21:05:23 +0600

    verson 1.0.3

--- archivehb/urls.py
+++ archivehb/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secretadmin/', admin.site.urls),
     path('', views.home, name='home'),
     path('note/', include('note.urls')),
     path('quiz/', include('quiz.urls')),
